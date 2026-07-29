ddc205309e30612bb4a29b9532579ebfb22acfec
Kamsi-ai <mkanugom@gmail.com>
2026-07-28 20:15:06 -0600

    sebastian commit create files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous_starter_bot_code.java
@@ -0,0 +1,164 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Robot: test", group="Robot")
+
+public class autonomous_starter_bot_code extends LinearOpMode{
+
+
+
+    private DcMotor intakeMotor;
+    private boolean toggleintakeforward = false;
+    private boolean toggleintakereverse = false;
+    private CRServo servoLeft;
+    private CRServo servoRight;
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+
+    private double leftServoPower= 1;
+    private double rightServoPower = 1;
+
+    private double fDS = 0;
+
+
+    private double driveMotorMaxPower = 1.0;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    static final double FORWARD_SPEED = 0.5;
+    static final double TURN_SPEED = 0.4;
+    static final double INTAKE_SPEED = 1;
+    static final double backward_speed = -0.5;
+
+
+
+    public void runOpMode() {
+
+        intakeMotor = hardwareMap.get(DcMotor.class, "Intake");
+        servoLeft = hardwareMap.get(CRServo.class, "sL");
+        servoRight = hardwareMap.get(CRServo.class, "sR");
+        leftDrive = hardwareMap.get(DcMotor.class, "bLD");
+        rightDrive = hardwareMap.get(DcMotor.class, "bRD");
+
+
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+
+
+        // Step through each leg of the path, ensuring that the OpMode has not been stopped along the way.
+
+
+            leftDrive.setPower(FORWARD_SPEED);
+        rightDrive.setPower(FORWARD_SPEED);
+        intakeMotor.setPower(INTAKE_SPEED);
+        servoLeft.setPower(leftServoPower);
+        servoRight.setPower(rightServoPower);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 3.0)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        leftDrive.setPower(fDS);
+        rightDrive.setPower(fDS);
+        intakeMotor.setPower(fDS);
+        servoLeft.setPower(fDS);
+        servoRight.setPower(fDS);
+
+
+       leftDrive.setPower(backward_speed);
+       rightDrive.setPower(backward_speed);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.3)) {
+            telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        leftDrive.setPower(fDS);
+        rightDrive.setPower(fDS);
+
+
+        leftDrive.setPower(TURN_SPEED);
+        rightDrive.setPower(-TURN_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.3)) {
+            telemetry.addData("Path", "Leg 4: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        leftDrive.setPower(fDS);
+        rightDrive.setPower(fDS);
+
+        /*
+        leftDrive.setPower(FORWARD_SPEED);
+        rightDrive.setPower(FORWARD_SPEED);
+        intakeMotor.setPower(INTAKE_SPEED);
+        servoLeft.setPower(leftServoPower);
+        servoRight.setPower(rightServoPower);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.5)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        leftDrive.setPower(fDS);
+        rightDrive.setPower(fDS);
+        intakeMotor.setPower(fDS);
+        servoLeft.setPower(0);
+        servoRight.setPower(0);
+
+
+        leftDrive.setPower(backward_speed);
+        rightDrive.setPower(backward_speed);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.3)) {
+            telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+
+        leftDrive.setPower(-TURN_SPEED);
+        rightDrive.setPower(TURN_SPEED);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 1.3)) {
+            telemetry.addData("Path", "Leg 4: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+
+        leftDrive.setPower(FORWARD_SPEED);
+        rightDrive.setPower(FORWARD_SPEED);
+        intakeMotor.setPower(INTAKE_SPEED);
+        servoLeft.setPower(leftServoPower);
+        servoRight.setPower(rightServoPower);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < 2.5)) {
+            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        intakeMotor.setPower(0);
+        servoLeft.setPower(0);
+        servoRight.setPower(0);
+
+         */
+        }
+    }
